fa805e75d00769c3e95cc2098068de45d13c028b
PranovAdilet <pranov.adiletqwe@mail.ru>
2024-02-18 19:38:39 +0600

    added phoneInput

---
+++ src/components/PhoneInput/Input.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'react-phone-number-input/style.css';
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input';
+
+
+type IProps = {
+    error: string
+    setError: (e: string) => void
+    tel: string
+    setTel: (e: string) => void
+}
+const Input = ({...props} : IProps) => {
+
+    const handleInputChange = (newValue: string | undefined) => {
+        props.setTel(newValue ?? '');
+    }
+
+    const handleBlur = () => {
+        if (props.tel && !isValidPhoneNumber(props.tel)){
+            props.setError('Invalid phone number format');
+        } else {
+            props.setError('');
+        }
+    }
+
+
+    return (
+        <div>
+            <PhoneInput
+                international
+                className="popup__input"
+                placeholder="Enter phone number"
+                value={props.tel}
+                onChange={handleInputChange}
+                defaultCountry="KG"
+                onBlur={handleBlur}
+                limitMaxLength={true}
+            />
+            {props.error && <p className="popup__error">{props.error}</p>}
+        </div>
+    );
+};
+
+export default Input;
--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import { FaRegUser } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
+import Input from "../PhoneInput/Input";
+import {isValidPhoneNumber} from "react-phone-number-input";
 
 type IPopup = {
     setPopup: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const Popup = ({setPopup}: IPopup) => {
+
+    const [tel, setTel] = useState<string>('');
+    const [error, setError] = useState<string>('')
+
+    const [count, setCount] = useState<number>(1)
+    const [inputText, setInputText] = useState<string>('')
+
+    const handlerCountPlus = () => {
+        if (count < 5){
+            setCount((prev) => prev + 1)
+        }
+    }
+    const handlerCountMinus = () => {
+        if (count > 1){
+            setCount((prev) => prev - 1)
+        }
+    }
+
     const handlerPopup = ()  => {
         setPopup(prev => !prev)
     }
 
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setCount(1)
+        setTel('')
+        setInputText('')
+    }
+
+    const handleChangeInputText = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value)
+    }
+
     return (
        <div className="overlay">
-           <div className="popup">
+           <form onSubmit={onSubmit} className="popup">
                <div className="popup__top">
                    <h2 className="popup__title">Info</h2>
                    <span onClick={handlerPopup} className="popup__icon-close"><IoClose/></span>
@@ -23,28 +54,36 @@ const Popup = ({setPopup}: IPopup) => {
                </p>
                <label className="popup__label">
                    <p className="popup__text">Phone number</p>
-                   <input className="popup__input" type="number"/>
+                   <Input tel={tel} setTel={setTel} error={error} setError={setError}/>
                </label>
                <label className="popup__label">
                    <p className="popup__text">Commentaries to trip</p>
-                   <input placeholder="Write your wishes to trip..." className="popup__input" type="text"/>
+                   <input
+                       minLength={3}
+                       required={true}
+                       placeholder="Write your wishes to trip..."
+                       className="popup__input"
+                       type="text"
+                       value={inputText}
+                       onChange={handleChangeInputText}
+                   />
                </label>
                <div className="popup__bottom">
                    <p className="popup__text">Commentaries to trip</p>
                    <div className="popup__bottom-block">
                        <div className="popup__bottom-block-count">
-                           <span className="popup__icon-minus">-</span>
-                           <p className="popup__bottom-count">5</p>
-                           <span className="popup__icon-plus">+</span>
+                           <span onClick={() => handlerCountMinus()} className="popup__icon-minus">-</span>
+                           <p className="popup__bottom-count">{count}</p>
+                           <span onClick={() => handlerCountPlus()} className="popup__icon-plus">+</span>
                        </div>
                        <div className="popup__bottom-block-people">
                            <span className="popup__icon-user"><FaRegUser/></span>
-                           <p className="popup__bottom-text">5 People</p>
+                           <p className="popup__bottom-text">{count} People</p>
                        </div>
                    </div>
                </div>
-               <button className="popup__btn">Submit</button>
-           </div>
+               <button disabled={!tel || !isValidPhoneNumber(tel) || inputText.length < 3} type="submit" className="popup__btn">Submit</button>
+           </form>
        </div>
     );
 };
